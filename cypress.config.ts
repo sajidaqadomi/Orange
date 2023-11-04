c61c21f7e55b57a8903ba266a93f76a808b3ef7b
sajidaqadomi <sajidaqadomi@outlook.sa>
2023-11-05 01:36:22 +0200

    Initit Orange: Setup project structure and basic files

---
+++ cypress.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "cypress";
+import allureWriter from "@shelex/cypress-allure-plugin/writer";
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php",
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+    env: {
+      download_dir: "./cypress/downloads",
+      allure: true,
+      allureResultsPath: "task-results",
+      screenshotsFolder: "task-results",
+      snapshotOnly: true,
+    },
+    reporterOptions: {
+      types: ["@shelex/cypress-allure-plugin"],
+      embeddedScreenshots: true,
+      inlineAwssets: true,
+    },
+    videosFolder: "task-results",
+    screenshotOnRunFailure: true,
+  },
+});
